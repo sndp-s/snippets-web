5f07701527bb7b6e452f69bbd8696da9f5e8b652
Sandeep Sharma <sandeeptech8@gmail.com>
2025-10-14 21:01:22 +0530

    feat: add toaster to app root

--- app/routes/snippets-app.tsx
+++ app/routes/snippets-app.tsx
@@ -1,5 +1,10 @@
+import { Toaster } from "~/components/ui/sonner";
+
 export default function SnippetsApp() {
   return (
-    <p>Snippets</p>
+    <>
+      <p>Snippets</p>
+      <Toaster position="top-right" richColors closeButton />
+    </>
   );
 };
