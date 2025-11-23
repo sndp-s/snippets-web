c24f87d7e8548aa35ac5ca366b37e161697c287b
Sandeep Sharma <sandeeptech8@gmail.com>
2025-11-23 14:32:31 +0530

    feat: add tanstack query client provider

--- app/components/ui/UIRoot.tsx
+++ app/components/ui/UIRoot.tsx
@@ -2,6 +2,9 @@ import type { ReactNode } from "react";
 import { TooltipProvider } from "~/components/ui/tooltip";
 import { Toaster } from "~/components/ui/sonner";
 import { SnippetDialog } from "~/components/snippet-dialog";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 interface UIRootProps {
   children: ReactNode;
@@ -9,10 +12,12 @@ interface UIRootProps {
 
 export default function UIRoot({ children }: UIRootProps) {
   return (
-    <TooltipProvider delayDuration={200}>
-      {children}
-      <Toaster richColors position="top-right" />
-      <SnippetDialog />
-    </TooltipProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider delayDuration={200}>
+        {children}
+        <Toaster richColors position="top-right" />
+        <SnippetDialog />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
